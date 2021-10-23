2e5566248eb14af0ed1482a9ef3bbaab54ff082d
Elektrosonikai <stobbe@unbc.ca>
2021-10-23 11:44:47 -0700

    Added data files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("Please enter input file name: ");
+        String dataFileName = inputReader.nextLine();
+        File dataFile = new File(dataFileName);
+
+        System.out.println("Simulation begins... ");
     }
 }
