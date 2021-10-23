018eb968a94b0535d9252a0b4a28b604c9a900b2
Elektrosonikai <stobbe@unbc.ca>
2021-10-23 16:07:51 -0700

    Changed AssessmentQueue to LinearQueue. Added comments to ArrivalEvent

--- src/ArrivalEvent.java
+++ src/ArrivalEvent.java
@@ -1,3 +1,14 @@
+/**
+ * This file is part of a solution to
+ *		CPSC300 Assigment 1 Problem 1 Fall 2021
+ *
+ * This event occurs when patients first arrive at the hospital
+ *
+ * @author Sebastian Lopez
+ * Student Number: 230140171
+ * @version 1
+ */
+
 import java.util.Random;
 
 public class ArrivalEvent extends Event{
@@ -23,6 +34,11 @@ public class ArrivalEvent extends Event{
         start(priority);
     }
 
+    /**
+     * Create a new Patient
+     * @param priority The severity of the patient's symptoms, used to determine which patients
+     *                 are to be treated earliest
+     */
     public void start(int priority){
         super.start();
         new Patient(creationTime, patientType, treatmentTime, priority);
--- src/DepartureEvent.java
+++ src/DepartureEvent.java
@@ -1,3 +1,14 @@
+/**
+ * This file is part of a solution to
+ *		CPSC300 Assigment 1 Problem 1 Fall 2021
+ *
+ * This event occurs when patients depart from the hospital
+ *
+ * @author Sebastian Lopez
+ * Student Number: 230140171
+ * @version 1
+ */
+
 public class DepartureEvent extends Event {
 
     private static final int DEPARTURE_PROCESSING_TIME = 1;
