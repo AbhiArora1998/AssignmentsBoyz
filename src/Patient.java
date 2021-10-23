d068438778e0697973ffec9d6b8a2aaf8329282f
Loppersy <Loppersy7@gmail.com>
2021-10-23 11:53:48 -0700

    Patient class updated

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Patient {
 
     public static final int CODE_MISSING = 0, CODE_W = 1, CODE_E = 2;
@@ -5,13 +7,18 @@ public class Patient {
     private int patientNumber = 0;
     private int code = 0;
     private int priority = 0;
+    private int treatmentTime = 0;
+    static Random random = new Random(1000);
 
-    public Patient(int patientNumber, int code) {
+    public Patient(int patientNumber, int code, int treatmentTime) {
         this.code = code;
         this.patientNumber = patientNumber;
+        this.treatmentTime = treatmentTime;
 
         if(code == CODE_E){
             priority = 1;
+        } else {
+            priority = random.nextInt(5)+1;
         }
     }
 
