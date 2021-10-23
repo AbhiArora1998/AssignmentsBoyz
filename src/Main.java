543bd5e9f3e541b472edcd1c621be65d2ee78cdb
MrBoberto <60299025+MrBoberto@users.noreply.github.com>
2021-10-23 12:25:29 -0700

    little problem

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,26 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws FileNotFoundException {
         Scanner inputReader = new Scanner(System.in);
-        System.out.println("Please enter input file name: ");
-        String dataFileName = inputReader.nextLine();
-        File dataFile = new File(dataFileName);
-
-        if (dataFile.canRead()) {
-            try {
-                Scanner in = new Scanner(dataFile);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        boolean filework = false;
+        Scanner fileInput = new Scanner(System.in);
+        while (!(filework)) {
+            System.out.println("Please enter input file name: ");
+            String dataFileName = inputReader.nextLine();
+            File dataFile = new File(dataFileName);
+            if (dataFile.canRead()) {
+                fileInput = new Scanner(dataFile);
+                filework =true;
             }
         }
-        int patientStartNum = 28064212;
-        char patientType = ' ';
-        int timeOfPatient =0;
-        String patientString = in.nextLine();
+            int patientStartNum = 28064212;
+            char patientType = ' ';
+            int timeOfPatient = 0;
+            String patientString = fileInput.nextLine();
 
-        System.out.println("Simulation begins... ");
+            System.out.println("Simulation begins... ");
+
 
     }
 }
