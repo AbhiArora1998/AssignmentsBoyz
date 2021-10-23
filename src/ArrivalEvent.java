fc64d4036a111665597cc54b92e4c1e3ef9730b7
Loppersy <Loppersy7@gmail.com>
2021-10-23 12:23:39 -0700

    Patient class update + Event + ArrivalEvent class

---
+++ src/ArrivalEvent.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class ArrivalEvent extends Event{
+
+    private final int time;
+    private final char code;
+    private final int treatmentTime;
+
+    private final Random random = new Random(1000);
+
+    public ArrivalEvent(int time, char code, int treatmentTime) {
+        this.time = time;
+        this.code = code;
+        this.treatmentTime = treatmentTime;
+    }
+
+    @Override
+    public void happens() {
+        new Patient(code, treatmentTime, random.nextInt(5)+1);
+    }
+}
--- src/Patient.java
+++ src/Patient.java
@@ -1,32 +1,38 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Patient {
 
-    public static final int CODE_MISSING = 0, CODE_W = 1, CODE_E = 2;
+    public static final char CODE_MISSING = 0, CODE_W = 'W', CODE_E = 'E';
     public static final int PRIORITY_MISSING = 0;
+
+    private static int nextPatientNumber = 28064212;
+    public static ArrayList<Patient> patients = new ArrayList<>();
+
     private int patientNumber = 0;
-    private int code = 0;
+    private char code = 0;
     private int priority = 0;
     private int treatmentTime = 0;
-    static Random random = new Random(1000);
 
-    public Patient(int patientNumber, int code, int treatmentTime) {
+
+    public Patient(char code, int treatmentTime, int priority) {
         this.code = code;
-        this.patientNumber = patientNumber;
+        this.patientNumber = nextPatientNumber++;
         this.treatmentTime = treatmentTime;
+        this.priority = priority;
 
         if(code == CODE_E){
             priority = 1;
-        } else {
-            priority = random.nextInt(5)+1;
         }
+
+        patients.add(this);
     }
 
     public int getPatientNumber() {
         return patientNumber;
     }
 
-    public int getCode() {
+    public char getCode() {
         return code;
     }
 
