12774d8b044b6d425b04e68092fabf9d6335ae97
Elektrosonikai <stobbe@unbc.ca>
2021-10-23 12:31:49 -0700

    Added WaitingQueue

---
+++ src/WaitingQueue.java
@@ -0,0 +1,13 @@
+/**
+ * This file is part of a solution to
+ *		CPSC300 Assigment 1 Problem 1 Fall 2021
+ *
+ * Implements the line for patients to wait to be treated in order of priority level
+ *
+ * @author Noah Stobbe
+ * Student Number: 230140171
+ * @version 1
+ */
+
+public class WaitingQueue {
+}
