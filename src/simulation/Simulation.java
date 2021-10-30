828e80c96341259649f6225c82d2725b4f201a00
MrBoberto <60299025+MrBoberto@users.noreply.github.com>
2021-10-30 15:00:53 -0700

    Added admitting to hospital event and changed simulation to reflect that

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -51,6 +51,7 @@ public final class Simulation {
         handleAssessmentEvents();
         handleStartTreatmentEvents();
         handleAdmittingToHospitalEvents();
+        handleDepartureEvents();
     }
 
     private static void handleAssessmentEvents() {
