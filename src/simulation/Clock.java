f15e5c11edb331bbb70483754f5fef9904cca595
Elektrosonikai <stobbe@unbc.ca>
2021-11-04 10:58:50 -0700

    Converted more normal comments in Clock to javadoc comments

--- src/simulation/Clock.java
+++ src/simulation/Clock.java
@@ -1,19 +1,31 @@
 package simulation;
 
+/**
+ * This file is part of a solution to
+ *		CPSC300 Assignment 1 Fall 2021
+ *
+ * Increments and keeps record of time in the simulation
+ *
+ * @author The Boyz
+ * @version 1
+ */
+
 public final class Clock {
     private static int time =0;
 
     private Clock(){}//Empty
-/*
-This is incrementing everyclock cycle
- */
+
+    /**
+     * This is incrementing everyclock cycle
+     */
     public static void advanceClock(){
         time++;
     }
-/*
-provides  us the current clock time
-@return the time variable whenever called
- */
+
+    /**
+     * provides  us the current clock time
+     * @return the time variable whenever called
+     */
     public static int getCurrentClockTime(){
         return time;
     }
